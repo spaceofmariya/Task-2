795c906dc9f6ed1fd2ed8d9b3a32dc955d49ae15
SpaceofMariya <silpamariyathampan@gmail.com>
2023-03-02 13:06:08 +0530

    right bar structuring continuation

--- src/components/includes/RightBar/RightBar.js
+++ src/components/includes/RightBar/RightBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './RightBar.css'
 import CalenderIcon from '../../../assets/images/Calender-Icon.svg'
 import DownArrow from '../../../assets/images/Down-Arrow.svg'
+import BreakFast1 from '../../../assets/images/Breakfast1.jpg'
+import Dollar from '../../../assets/images/Dollar.svg'
 
 function RightBar() {
   return (
@@ -15,6 +17,26 @@ function RightBar() {
         <div className='arrowBox'>
           <img src={DownArrow} alt="Down arrow" />
         </div>
+      </div>
+
+      <div className='meal'>
+        <div className='title'>
+          <h5>Breakfast</h5>
+          <span className='purchased'>Purchased</span>
+        </div> 
+        <span className='time'>06.30AM</span>
+        <ul className='foodItem'>
+          <li>
+            <div className='foodBox'><img src={BreakFast1} alt="food" /></div>
+            <div className='textBox'>
+              <h6>Nasi Padang Patikraja</h6>
+              <div className='price'>
+                <span className='currentPrice'><span className='dollarBox'><img src={Dollar} alt="dollar icon" /></span>8,10</span>
+                <span className='initialPrice'>$10,10</span>
+                </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   )
