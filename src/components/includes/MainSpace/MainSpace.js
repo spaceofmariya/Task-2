782edc267418ef3ad3caf14edc788cb6accdcc75
SpaceofMariya <silpamariyathampan@gmail.com>
2023-03-02 20:57:52 +0530

    header structure and style completed

--- src/components/includes/MainSpace/MainSpace.js
+++ src/components/includes/MainSpace/MainSpace.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './MainSpace.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { MenuData } from '../Data'
 import { ReactComponent as SearchIcon } from '../../../assets/images/Search.svg'
 
@@ -12,14 +12,22 @@ function MainSpace() {
         <h1>Welcome, Epan Danis!</h1>
         <span>Don't forget to have breakfast today yeahhh!</span>
       </div>
+      
+      <nav className='menu'>
+        <ul className='menuContainer'>
+          <li className='menuBox' style={{ backgroundColor:'#f46801' }}><NavLink to="/" style={{ color:"#fff" }}>Menu</NavLink></li>
+          <li className='menuBox' style={{ backgroundColor:'#eeeeee' }}><NavLink to="/" style={{ color:"#777" }}>Meal plans</NavLink></li>
+          <li className='menuBox' style={{ backgroundColor:'#eeeeee' }}><NavLink to="/" style={{ color:"#777" }}>Delivery</NavLink></li>
+        </ul>
+      </nav>
 
-      <nav className='menuBox'>
+      {/* <nav className='menuBox'>
             <ul className="menu">
               <li style={{ backgroundColor:'#f46801' }}><a href="/" style={{ color:"#fff" }} >Menu</a></li>
               <li style={{ backgroundColor:'#eeeeee' }}><a href="/" style={{ color:'#777' }}>Meal plans</a></li>
               <li style={{ backgroundColor:'#eeeeee' }}><a href="/" style={{ color:'#777' }}>Delivery</a></li>
             </ul> 
-            </nav> 
+            </nav>  */}
      
       <div className='search'><span className='searchText'>Search your food...</span><span className='searchIcon'><SearchIcon /></span></div>
 
